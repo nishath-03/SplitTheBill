455ea1ae830c217609bc7c57ba87db0dee7b12e6
Nishath Arumugam <nishatharumugam3@gmail.com>
2026-09-18 21:45:59 +0530

    -

--- src/main/java/com/hotelsplit/exception/ConflictException.java
+++
@@ -1,5 +0,0 @@
-package com.hotelsplit.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) { super(message); }
-}
--- src/main/java/com/hotelsplit/exception/UnauthorizedException.java
+++
@@ -1,5 +0,0 @@
-package com.hotelsplit.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) { super(message); }
-}
